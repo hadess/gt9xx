8c28d7315e522eac23beeaa39ad704632656aee6
Bastien Nocera <hadess@hadess.net>
2014-09-24 16:33:32 +0200

    Report errors in goodix_i2c_write() as in goodix_i2c_read()

--- goodix.c
+++ goodix.c
@@ -117,7 +117,7 @@ static int goodix_i2c_write(struct i2c_client *client,
 
 	ret = i2c_transfer(client->adapter, &msg, 1);
 	kfree(wbuf);
-	return ret;
+	return ret < 0 ? ret : (ret != ARRAY_SIZE(msgs) ? -EIO : 0);
 }
 
 static int goodix_ts_read_input_report(struct goodix_ts_data *ts, u8 *data)
